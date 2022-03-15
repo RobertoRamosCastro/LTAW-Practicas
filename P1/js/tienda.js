c0a49fa3e2e6bc5ec44c219f7f8767d11ebdd2f9
Roberto <r.ramos.2017>
2022-03-15 19:06:24 +0100

    condiciones

--- P1/js/tienda.js
+++ P1/js/tienda.js
@@ -4,3 +4,17 @@ const http = require('http');
 const fs = require('fs');
 // escuchamos en puerto 7007
 const PUERTO = 7007; 
+
+const server = http.createServer(function(req, res) {
+
+    console.log("\nPetición recibida!");
+    let myURL = new URL(req.url, 'http://' + req.headers['host']);
+    console.log("La URL del recurso solicitado es: " + myURL.href)
+    let solicitud = "";
+    if(myURL.pathname == '/') { 
+      solicitud += "/web.html" 
+    }else if(myURL.pathname == "/favicon.icon"){
+      filename = 'imagenes/ico.ico'
+    }else {
+      solicitud = myURL.pathname;
+    }
