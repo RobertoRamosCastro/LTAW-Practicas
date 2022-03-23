256336631854fb562bd29988e0a0b1acf1a351ac
Roberto <r.ramos.2017>
2022-03-23 19:09:44 +0100

    arreglos, usamos la var q antes estaba sin usar

--- P1/js/main.js
+++ P1/js/main.js
@@ -1,5 +1,11 @@
 function main() {
 
+    // Obtenemos los audios
+    var audiosable = document.getElementById("audiosable");
+    var audioblaster = document.getElementById("audioblaster");
+    var audioxiwing = document.getElementById("audioXWing");
+    var audiocarrito = document.getElementById("audiocarrito");
+
     // Obtenemos los productos
     var sable = document.getElementById("sable");
     var blaster = document.getElementById("blaster");
@@ -9,6 +15,24 @@ function main() {
     // Obtengo el banner
     var banner = document.getElementById("img");
     var section_banner = document.getElementById("banner");
+
+    // Activamos la reproducción de los audios al pasar el mouse por encima
+    sable.onmouseover = () => {
+        audiosable.play();
+    }
+
+    blaster.onmouseover = () => {
+        audioblaster.play();
+    }
+
+    xwing.onmouseover = () => {
+        audioxiwing.play();
+    }
+
+    // Activamos el audio al hacer click
+    carrito.onclick = () => {
+        audiocarrito.play();
+    }
 
     // Activamos temporizador para que cada 10 segundos cambie la imagen del banner
     setTimeout( () =>{
--- P1/js/main2.js
+++ P1/js/main2.js
@@ -1,5 +1,11 @@
 function main() {
 
+    // Obtenemos los audios
+    var audioxwing = document.getElementById("audioxwing");
+    var audiosable = document.getElementById("audiosable");
+    var audiocarrito = document.getElementById("audiocarrito");
+    var audiocompra =document.getElementById("audiocompra");
+
     // Obtenemos los productos
     var xwing = document.getElementById("X-Wing");
     var sable = document.getElementById("sable");
