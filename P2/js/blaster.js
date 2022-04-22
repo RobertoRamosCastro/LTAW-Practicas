820b45227688a68680177439e8e7834b30e0f5e1
Roberto <r.ramos.2017>
2022-04-22 17:47:01 +0200

    funcion main implementada

---
+++ P2/js/blaster.js
@@ -0,0 +1,14 @@
+function main() {
+
+    // Obtenemos los audios
+    var audioxwing = document.getElementById("audioxwing");
+    var audiosable = document.getElementById("audiosable");
+
+    // Obtenemos los productos
+    var xwing = document.getElementById("X-Wing");
+    var sable = document.getElementById("sable");
+
+    //Obtengo la caja de búsqueda y el display
+    var caja = document.getElementById("caja_busqueda");
+    var display = document.getElementById("display");
+}
