08cb14bb0d860f1b23e67f15a40bd74008ca6de7
Roberto <r.ramos.2017>
2022-04-23 17:20:14 +0200

    variables

---
+++ P2/js/main.js
@@ -0,0 +1,20 @@
+function main() {
+
+    // Obtenemos los audios
+    var audiosable = document.getElementById("audiosable");
+    var audioblaster = document.getElementById("audioblaster");
+    var audioxiwing = document.getElementById("audioXWing");
+
+    // Obtenemos los productos
+    var sable = document.getElementById("sable");
+    var blaster = document.getElementById("blaster");
+    var xwing = document.getElementById("XWing");
+
+    // Obtengo el banner
+    var banner = document.getElementById("img");
+    var section_banner = document.getElementById("banner");
+
+    //Obtengo la caja de búsqueda y el display
+    var caja = document.getElementById("caja_busqueda");
+    var display = document.getElementById("display");
+}
