21d3e6f0da7ca76152c9be965be1bd4f35d12dbc
Roberto <r.ramos.2017>
2022-05-19 22:38:24 +0200

    recibir y reenviar mensajes de los clientes conect

--- P3/js/chat_server.js
+++ P3/js/chat_server.js
@@ -68,6 +68,72 @@ io.on('connect', (socket) => {
     io.emit('hide', data)
   });  
   
+
+
+  //-- Mensaje recibido: Reenviarlo a todos los clientes conectados
+  socket.on("message", (msg)=> {
+    console.log("Mensaje Recibido!: " + msg.blue);
+    var socketId = socket.id;
+    console.log("socket id: " + socketId.green + " " +socket.username);
+
+    if (msg=='/help') {
+        console.log("Mensaje de ayuda".red)
+        msg = '/help: Devuelve la lista con todos los comandos' + "<br>" +
+        '/list : Devuelve el numero de usuarios conectados' + "<br" +
+        '/hello : Devuelve el saludo del servidor' + "<br>" +
+        '/date : Devuelve la fecha' + "<br>" +
+        '/"nombre": envía un mensaje privado al usuario'
+
+        io.to(socketId).emit('server_msg', msg);
+    } else if (msg == '/hello'){
+        console.log("Mensaje de saludo".red)
+        msg = '¡HOLI!';
+        console.log(socketId);
+        io.to(socketId).emit('server_msg', msg);
+    }  else if (msg == '/date'){
+        var d = new Date();
+        var yy = d.getFullYear();
+        var mm = d.getMonth();
+        var dd = d.getDate();
+        msg = 'Fecha: ' + dd + '/' + mm + '/' + yy;
+        io.to(socketId).emit('server_msg', msg);
+    } else if (msg == '/list') {
+      msg = 'Actualmente hay ' + users + " usuarios conectados. <br>"
+      console.log(dict)
+      // Recorro el diccionario de usuarios
+      for (i=0; i< dict.length; i++){
+        msg += "> " + (dict[i].name) + "<br>"
+      }
+      io.to(socketId).emit('server_msg', msg);
+    } else if (msg.split("/")[0] == "") {
+      // msg.split("/")[0] --> " "
+      // msgsplit("/")[1] --> "username + mensaje"
+      console.log("Es un mensaje privado")
+
+      // separo el usuario del mensaje
+      var index = msg.split("/")[1].indexOf(" ")
+      var new_msg = msg.slice(index + 1)
+      var user2priv = msg.split("/")[1].slice(0,index)
+
+      for (i=0; i< dict.length; i++){
+        if (dict[i].name == user2priv) {
+          var priv_id = dict[i].id;
+          break;
+        }
+      }
+        console.log("MENSAJE A: " + user2priv.red + " con id: " + priv_id.green)
+       // Me quedo solo con el mensaje haciendo un split [0] --> /username
+       // [1] --> mensaje 
+        io.to(priv_id).emit('priv', socket.username + ": " + new_msg);
+        io.to(socketId).emit('priv', socket.username + ": " + new_msg);
+    } else {
+      //-- Reenviarlo a todos los clientes conectados
+          io.send(socket.username + ": " + msg);
+    }
+  });
+
+});
+
 //-- Lanzar el servidor HTTP
 //-- ¡Que empiecen los juegos de los WebSockets!
 server.listen(PUERTO);
