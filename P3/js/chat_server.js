7974c4f6bdaa68111d04b88f4ca30b2d1c3a601c
Roberto <r.ramos.2017>
2022-05-19 22:36:44 +0200

    cts + pnto de entrada a la app

---
+++ P3/js/chat_server.js
@@ -0,0 +1,37 @@
+//-- Cargar las dependencias
+const socket = require('socket.io');
+const http = require('http');
+const express = require('express');
+const colors = require('colors');
+const fs = require('fs');
+
+const FICHERO_HTML = 'client.html';
+const HTML = fs.readFileSync(FICHERO_HTML, 'utf-8');
+
+// Control de número de usuarios
+var users = 0;
+var dict = [];
+
+const PUERTO = 8080;
+
+//-- Crear una nueva aplciacion web
+const app = express();
+
+//-- Crear un servidor, asosiaco a la App de express
+const server = http.Server(app);
+
+//-- Crear el servidor de websockets, asociado al servidor http
+const io = socket(server);
+
+//-------- PUNTOS DE ENTRADA DE LA APLICACION WEB
+//-- Definir el punto de entrada principal de mi aplicación web
+app.get('/', (req, res) => {
+  res.redirect('/client.html')
+});
+
+//-- Esto es necesario para que el servidor le envíe al cliente la
+//-- biblioteca socket.io para el cliente
+app.use('/', express.static(__dirname +'/'));
+
+//-- El directorio publico contiene ficheros estáticos
+app.use(express.static('public'));
