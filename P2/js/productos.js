659614a303d31e0c5a100489acb1802e70d79819
Roberto <r.ramos.2017>
2022-05-20 12:25:02 +0200

    main que modifica la imagen no seleccionada

--- P2/js/productos.js
+++ P2/js/productos.js
@@ -14,3 +14,29 @@ function send_cookie(){
   document.cookie = "carrito=" + final_cookie + "; path=/ ; Secure";
   console.log(items);
 }
+
+function main() {
+  console.log("Ejecutando js")
+
+  for (var i = 0; i < imgs.length; i++) {
+    imgs[i].onclick = (ev) => {
+        console.log(ev.target.height);
+        img = ev.target;
+        img.style.border = '0px'
+        canvas.height = 250;
+        canvas.width = 180;
+        ctx.drawImage(img, 0,0);
+        console.log('click')
+        // Quito el borde de la imagen no seleccionada
+        for (var i = 0; i < imgs.length; i++) {
+          if (imgs[i].style.border = 'rgba(0,0,0,0.75)') {
+            imgs[i].style.backgroundColor = 'rgba(0,0,0,0)'
+            imgs[i].style.border = 'silver 10px solid'
+            imgs[i].style.borderImageSlice = '27 fill'
+            imgs[i].style.borderImageSource = 'url(../img/border.png)'
+          }
+        }
+    console.log("Imagen lista...");
+  }
+
+}
