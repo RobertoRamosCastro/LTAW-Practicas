a5bd839286018708754d647da4885df06751c234
Roberto <r.ramos.2017>
2022-05-21 21:23:22 +0200

    sockets añadidos

--- P4/js/client.js
+++ P4/js/client.js
@@ -38,5 +38,62 @@ function main() {
 //-- Crear un websocket. Se establece la conexión con el servidor
 const socket = io();
 
+function typingstopped(){
+  typing = false;
+  socket.emit('notTyping');
+}
 
+function onKeyDown(){
+  if(typing == false) {
+    typing = true
+    socket.emit('typing', user );
+  } 
+}
 
+socket.on('display', (data) => {
+  if (data != user && nickname) {
+    typing_msg[0].innerHTML = data + " is typing";
+  }
+  
+});
+
+socket.on('hide', (data) => {
+  setTimeout(function(){ 
+    typing_msg[0].innerHTML = ""; }, 1500);
+  
+});
+
+socket.on("message", (msg)=>{
+
+  var user_msg = msg.split(":")[0]
+  if (nickname) {
+    if (user == user_msg) {
+      console.log("PRUEBA --> " + msg.split(":")[0] + msg )
+      display.innerHTML += '<p id="display_user">' + msg + '</p>'; 
+    } else {
+      play_sound(sound_msg);
+      display.innerHTML += '<p>' + msg + '</p>'; 
+    }
+     
+  }
+});
+
+socket.on("priv", (msg)=>{
+
+  var user_msg = msg.split(":")[0]
+  if (nickname) {
+    if (user == user_msg) {
+      display.innerHTML += '<p id="priv_user">' + msg + '</p>'; 
+    } else {
+      play_sound(sound_msg);
+      display.innerHTML += '<p style="color:purple;">' + msg + '</p>'; 
+    }
+     
+  }
+});
+
+socket.on("server_msg", (msg)=>{
+
+    play_sound(sound_msg);
+    display.innerHTML += '<p class="server_msg">' + msg + '</p>'; 
+});
